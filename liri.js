e857da9e8b7705045ad7c15be5417cb794998eb4
Kirsten Millan <poppy0m9@gmail.com>
2018-10-08 21:28:18 -0400

    added package.json

---
+++ liri.js
@@ -0,0 +1,14 @@
+```js
+require("dotenv").config();
+```
+var inquirer = require('inquirer');
+inquirer.prompt([/* Pass your questions in here */]).then(answers => {
+    // Use user feedback for... whatever!!
+});
+
+var keys = require("keys.js");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+var fs = require('fs');
+
